dd7d0739c45595c136547026c776e255f31ee50b
sagarjain0456 <72862757+sagarjain0456@users.noreply.github.com>
2022-05-15 13:04:47 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,60 @@
+$('.owl-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true
+})
+
+$('.owl-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+autoplayTimeout:3000,
+autoplayHoverPause:true,
+responsiveClass:true,
+responsive:{
+    0:{
+        items:1,
+        nav:true
+    },
+    600:{
+        items:2,
+        nav:false
+    },
+    925:{
+        items:4,
+        nav:true,
+        loop:false
+    }
+}
+});
+setTimeout(function(){
+    document.getElementById('navbar').setAttribute("style","transform:translateY(0px);")
+    }, 50);
+
+    setTimeout(function(){
+        document.getElementById('about-krmu-text').setAttribute("style","transform:translateX(0px);")
+        }, 50);
+
+        setTimeout(function(){
+            document.getElementById('about-krmu-image').setAttribute("style","transform:translateX(0px);")
+            }, 50);
+
+window.onscroll = function(){
+    //console.log(window.scrollY);
+    //console.log(document.getElementById('upcoming-events').scrollHeight);
+
+    var ans = ((window.scrollY/document.getElementById('our-mission').offsetTop)+0.2).toFixed(2);
+   // console.log(ans);
+    document.getElementById('our-mission').style.opacity = ans;
+
+    var ans2 = ((window.scrollY/document.getElementById('upcoming-events').offsetTop)+0.2).toFixed(2);
+    // console.log(ans);
+     document.getElementById('upcoming-events').style.opacity = ans2;
+ 
+    //console.log(ans2)
+  }
